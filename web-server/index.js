afb21fce506ac8d2ee38aaf9ab39b38b5064ff5d
tuan anh le <letuananh0301.dev@gmail.com>
2022-06-29 01:16:10 +0700

    dynamic page

--- web-server/index.js
+++ web-server/index.js
@@ -1,8 +1,35 @@
 /** @format */
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
+
+const publicDirectoryPath = path.join(__dirname, './public');
+
+app.set('view engine', 'hbs');
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('home', {
+    title: 'Home',
+    name: 'Tuan Anh',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'Tuan Anh',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'Tuan Anh',
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
