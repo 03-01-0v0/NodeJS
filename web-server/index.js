4605154f39b3d0fbe6862616971dcdcb3b513f3e
tuan anh le <letuananh0301.dev@gmail.com>
2022-06-30 01:08:34 +0700

    query string get api with web-server

--- web-server/index.js
+++ web-server/index.js
@@ -2,19 +2,60 @@
 
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
+const geocode = require('./src/utils/geocode');
+const forecast = require('./src/utils/forecast');
 const app = express();
 const port = 3000;
 
 const publicDirectoryPath = path.join(__dirname, './public');
+const viewsPath = path.join(__dirname, './templates/views');
+const partialsPath = path.join(__dirname, './templates/partials');
 
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 app.use(express.static(publicDirectoryPath));
+hbs.registerPartials(partialsPath);
 
 app.get('', (req, res) => {
   res.render('home', {
     title: 'Home',
     name: 'Tuan Anh',
   });
+});
+
+app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address',
+    });
+  }
+  geocode(req.query.address, (err, data) => {
+    if (err) {
+      return res.send({ err });
+    } else {
+      forecast(
+        data[0].geometry.coordinates[0],
+        data[0].geometry.coordinates[1],
+        (error, dataForecast) => {
+          if (error) {
+            return res.send({ error });
+          } else {
+            return res.send({
+              data,
+              dataForecast,
+              address: req.query.address,
+            });
+          }
+        }
+      );
+    }
+  });
+  // res.send({
+  //   forecast: 'It is snowing',
+  //   location: 'Philadelphia',
+  //   address: req.query.address,
+  // });
 });
 
 app.get('/about', (req, res) => {
@@ -57,6 +98,22 @@ app.get('/about', (req, res) => {
 
 app.get('/weather', (req, res) => {
   res.send('Weather page');
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Tuan Anh',
+    errorMessage: 'Help article not found',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Tuan Anh',
+    errorMessage: 'Page not found',
+  });
 });
 
 app.listen(port, () => {
